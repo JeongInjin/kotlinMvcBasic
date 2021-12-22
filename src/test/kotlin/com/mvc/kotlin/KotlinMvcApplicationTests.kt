02ed499c1d3b7bab7de7a2d0e2c60fc8d35c2104
injinjeong <snowwwwww@naver.com>
2021-12-22 18:57:11 +0900

    Initial commit

---
+++ src/test/kotlin/com/mvc/kotlin/KotlinMvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mvc.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinMvcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
