8e5fc3b163141808d87e0385f0b8f5377aa42d99
injinjeong <snowwwwww@naver.com>
2021-12-25 09:58:35 +0900

    custom validation Annotation 추가

--- src/main/kotlin/com/mvc/kotlin/model/http/UserRequest.kt
+++ src/main/kotlin/com/mvc/kotlin/model/http/UserRequest.kt
@@ -3,6 +3,7 @@ package com.mvc.kotlin.model.http
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import com.mvc.kotlin.annotation.StringFormatDateTime
 import java.lang.Exception
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -31,15 +32,22 @@ data class UserRequest(
     @JsonProperty("phone_number")
     var phoneNumber : String? = null,
 
+    @field: StringFormatDateTime(pattern = "yyyy-MM-dd HH:mm:ss", message = "패턴이 올바르지 않습니다.")
     var createdAt: String? = null,
-){
-    @AssertTrue(message = "생성일자의 패턴은 yyyy-MM-dd HH:mm:ss 여야 합니다.")
-    private fun isValidCreateAt(): Boolean{
-        return try {
-            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-            true
-        }catch (e: Exception){
-            false
-        }
-    }
-}
+)
+/*
+@Size 문자 길이 측정 - Int Type 불가
+@NotNull null 불가
+@NotEmpty null, "" 불가
+@NotBlank null, "", " " 불가
+@Past 과거날짜
+@PastOrPresent 오늘이거나 과거 날짜
+@Future 미래 날짜
+@FutureOrPresent 오늘이거나 미래 날짜
+@Pattern 정규식 적용
+@Max 최대값
+@Min 최소값
+@AssertTrue / False - 별도 Logic 적용
+@Valid 해당 object validation 실행
+...
+ */
