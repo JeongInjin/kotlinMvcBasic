495a6849ed4913125c75530545908d88d88989e4
injinjeong <snowwwwww@naver.com>
2021-12-25 11:31:18 +0900

    exception advice init

---
+++ src/main/kotlin/com/mvc/kotlin/advice/GlobalControllerAdvice.kt
@@ -0,0 +1,25 @@
+package com.mvc.kotlin.advice
+
+import com.mvc.kotlin.controller.ExceptionApiController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+//@RestControllerAdvice
+//@ControllerAdvice
+@RestControllerAdvice(basePackageClasses = [ExceptionApiController::class])
+class GlobalControllerAdvice {
+
+    @ExceptionHandler(value = [RuntimeException::class])
+    fun exception(e: RuntimeException): String {
+        return "Server Error"
+    }
+
+    @ExceptionHandler(value = [IndexOutOfBoundsException::class])
+    fun indexOutOfBoundsException(e: IndexOutOfBoundsException): ResponseEntity<String>{
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Index ERROR")
+    }
+
+
+}
